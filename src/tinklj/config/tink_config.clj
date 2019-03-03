dcd0cb9f644faed49a2e15e12f7b7829823fc663
perkss <stuart.m.perks@gmail.com>
2019-03-03 08:36:54 +0000

    Initial clear keyset write and read

--- src/tinklj/config/tink_config.clj
+++ src/tinklj/config/tink_config.clj
@@ -1,5 +1,12 @@
 (ns tinklj.config.tink-config
-  (:import (com.google.crypto.tink.config TinkConfig)))
+  (:import (com.google.crypto.tink.config TinkConfig)
+           (com.google.crypto.tink Registry)))
 
-(defn register []
+(defn register
+  []
   (TinkConfig/register))
+
+(defn register-key-manager
+  "Register custom implementation of key manager"
+  [key-manager]
+  (Registry/registerKeyManager key-manager))
---
+++ src/tinklj/keysets/keyset_storage.clj
@@ -0,0 +1,16 @@
+(ns tinklj.keysets.keyset-storage
+  (:import (com.google.crypto.tink CleartextKeysetHandle JsonKeysetWriter JsonKeysetReader))
+  (:require [clojure.java.io :as io] ))
+
+
+(defn write-clear-text-keyset-handle
+  [keyset-handle filename]
+  (CleartextKeysetHandle/write
+    keyset-handle
+    (JsonKeysetWriter/withFile (io/file filename))))
+
+(defn load-clear-text-keyset-handle
+  [filename]
+  (CleartextKeysetHandle/read
+    (JsonKeysetReader/withFile
+      (io/file filename))))
