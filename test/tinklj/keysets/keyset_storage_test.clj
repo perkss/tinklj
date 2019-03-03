1a412d22fcd0a5bdeff9aa7ce20380b7dc157e5b
perkss <stuart.m.perks@gmail.com>
2019-03-03 08:50:38 +0000

    Add intiial storage test

---
+++ test/tinklj/keysets/keyset_storage_test.clj
@@ -0,0 +1,12 @@
+(ns tinklj.keysets.keyset-storage-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [tinklj.keys.keyset-handle :as keyset-handles]
+            [tinklj.keysets.keyset-storage :as keyset-storage]
+            [tinklj.config.tink-config :refer [register]]))
+
+(deftest write-clear-text-handle-test
+  (testing "Testing that a file is written and then read"
+    (register)
+    (let [keyset-handle (keyset-handles/generate-new keyset-handles/AES128-GCM)
+          filename "my-test-keyset"]
+      (keyset-storage/write-clear-text-keyset-handle keyset-handle filename))))
