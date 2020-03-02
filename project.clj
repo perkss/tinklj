52588771aa5878011a1d360ae20eafc1807075f3
lamp <matthew.gradidge@gmail.com>
2020-03-02 17:13:29 -0500

    Bump the version on tink

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tinklj "0.1.1-SNAPSHOT"
   :description "A Clojure API for Google Tink Crypto Library. Offering a range of cryptographic techniques that are simple and easy to use."
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.google.crypto.tink/tink "1.2.2"]]
+                 [com.google.crypto.tink/tink "1.3.0-rc4"]]
   :plugins [[lein-cljfmt "0.6.4"]]
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "0.0-389"]]}}
   :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
