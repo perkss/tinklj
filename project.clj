613ab894153576cfe9ba18eb214662a648aefb82
perkss <stuart.m.perks@gmail.com>
2023-01-04 22:29:30 +0000

    Upgrade to 1.7 tink

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject tinklj "0.1.6-SNAPSHOT"
+(defproject tinklj "0.1.7-SNAPSHOT"
   :description "A Clojure API for Google Tink Crypto Library. Offering a range of cryptographic techniques that are simple and easy to use."
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.google.crypto.tink/tink "1.6.1"]
+                 [com.google.crypto.tink/tink "1.7.0"]
                  ;; https://mvnrepository.com/artifact/com.google.crypto.tink/tink-awskms
-                 [com.google.crypto.tink/tink-awskms "1.6.1"]
+                 [com.google.crypto.tink/tink-awskms "1.7.0"]
                  ;; https://mvnrepository.com/artifact/com.google.crypto.tink/tink-gcpkms
-                 [com.google.crypto.tink/tink-gcpkms "1.6.1"]
+                 [com.google.crypto.tink/tink-gcpkms "1.7.0"]
                  [com.google.http-client/google-http-client-jackson2 "1.40.1"]]
   :repositories [["clojars" {:sign-releases false}]]
   :plugins [[lein-cljfmt "0.6.4"]]
